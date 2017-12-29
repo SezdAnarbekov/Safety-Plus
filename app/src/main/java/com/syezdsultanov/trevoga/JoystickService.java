16ced29e87508e711caffdad7a4c028e91adfc61
SezdAnarbekov <anarbekuulu@gmail.com>
2017-12-29 08:34:21 -0600

    added info

--- app/src/main/java/com/syezdsultanov/trevoga/JoystickService.java
+++ app/src/main/java/com/syezdsultanov/trevoga/JoystickService.java
@@ -1,18 +1,21 @@
 package com.syezdsultanov.trevoga;
 
 import android.Manifest;
+import android.app.DownloadManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.location.Location;
 import android.location.LocationManager;
+import android.media.MediaRecorder;
 import android.os.Build;
+import android.os.Environment;
 import android.os.IBinder;
-import android.provider.MediaStore;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.telephony.SmsManager;
@@ -20,28 +23,30 @@ import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Chronometer;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static android.view.WindowManager.LayoutParams;
 
 public class JoystickService extends Service {
 
-
+    private MediaRecorder myRecorder;
     private LocationManager mLocationManager;
     private WindowManager mWindowManager;
+    private LayoutParams params;
+    private LinearLayout mLinearLayout;
     private ImageView mJoystickImageView;
+    private Chronometer mChronometer;
     private long lastPressTime, pressTime;
-    private String sms, phoneNumber;
+    private String sms, phoneNumber, outputFile;
     private volatile boolean flag = true;
-
-    private static boolean isAvailable(Context ctx, Intent intent) {
-        final PackageManager mgr = ctx.getPackageManager();
-        List<ResolveInfo> list =
-                mgr.queryIntentActivities(intent,
-                        PackageManager.MATCH_ALL);
-        return list.size() > 0;
-    }
 
     @Nullable
     @Override
@@ -52,43 +57,57 @@ public class JoystickService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mJoystickImageView = new ImageView(this);
+        mJoystickImageView.setImageResource(R.drawable.sos_image_start);
+        mChronometer = new Chronometer(this);
+        mChronometer.setBase(SystemClock.elapsedRealtime());
+        mChronometer.setTextSize(15.0f);
+        mChronometer.setTextColor(Color.RED);
+        mChronometer.setVisibility(View.GONE);
+        int LAYOUT_FLAG;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LAYOUT_FLAG = LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            LAYOUT_FLAG = LayoutParams.TYPE_PHONE;
+        }
+        params = new LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT,
+                LAYOUT_FLAG,
+                LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP | Gravity.START;
+        mLinearLayout = new LinearLayout(this);
+        mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams LayoutParamsview = new LinearLayout.LayoutParams
+                (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        mLinearLayout.setGravity(Gravity.CENTER_HORIZONTAL);
+        mChronometer.setLayoutParams(LayoutParamsview);
+        mJoystickImageView.setLayoutParams(LayoutParamsview);
+        mLinearLayout.addView(mJoystickImageView);
+        mLinearLayout.addView(mChronometer);
+        params.x = 0;
+        params.y = 100;
+        mWindowManager.addView(mLinearLayout, params);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, final int startId) {
-        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-        mJoystickImageView = new ImageView(this);
-        mJoystickImageView.setImageResource(R.drawable.logo_sos_start);
-        int LAYOUT_FLAG;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
-        } else {
-            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
-        }
-
-        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                LAYOUT_FLAG,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
-        params.gravity = Gravity.TOP | Gravity.START;
-        params.x = 0;
-        params.y = 100;
-        mWindowManager.addView(mJoystickImageView, params);
         SharedPreferences pref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         phoneNumber = pref.getString("number", "");
         sms = pref.getString("text", "");
         if (sms.equals("")) {
-            sms = "Driver Pro\n";
+            sms = "Safety Plus\n";
         }
         mJoystickImageView.setOnTouchListener(new View.OnTouchListener() {
-            private final WindowManager.LayoutParams paramsF = params;
+            private final LayoutParams paramsF = params;
             private int initialX;
             private int initialY;
             private float initialTouchX;
             private float initialTouchY;
             private Thread t;
+            private boolean isCronometerStarted;
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -96,6 +115,17 @@ public class JoystickService extends Service {
                     case MotionEvent.ACTION_DOWN:
                         pressTime = System.currentTimeMillis();
                         if (pressTime - lastPressTime <= 300) {
+                            if (myRecorder != null) {
+                                myRecorder.stop();
+                                myRecorder.reset();
+                                myRecorder.release();
+                                myRecorder = null;
+                                saveToDownloads();
+                            }
+                            if (isCronometerStarted) {
+                                mChronometer.stop();
+                                mChronometer = null;
+                            }
                             JoystickService.this.stopSelf();
                             if (t != null) {
                                 flag = false;
@@ -109,38 +139,32 @@ public class JoystickService extends Service {
                         break;
                     case MotionEvent.ACTION_UP:
                         if ((System.currentTimeMillis() - pressTime) > 1000) {
-                            if (isAvailable(getApplicationContext(),
-                                    new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION))) {
-                                Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                startActivity(intent);
-                            } else if (isAvailable(getApplicationContext(),
-                                    new Intent(MediaStore.ACTION_VIDEO_CAPTURE))) {
-                                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                startActivity(intent);
-                            }
-                            mJoystickImageView.setImageResource(R.drawable.logo_sos);
+                            mJoystickImageView.setImageResource(R.drawable.sos_image);
+                            mChronometer.setVisibility(View.VISIBLE);
+                            mChronometer.start();
+                            isCronometerStarted = true;
                             t = new TheThread(startId);
                             t.start();
-//                            try {
-//                                t.join();
-//                            } catch (InterruptedException e) {
-//                                e.printStackTrace();
-//                            }
                         }
                         break;
                     case MotionEvent.ACTION_MOVE:
-
                         paramsF.x = initialX + (int) (event.getRawX() - initialTouchX);
                         paramsF.y = initialY + (int) (event.getRawY() - initialTouchY);
-                        mWindowManager.updateViewLayout(mJoystickImageView, paramsF);
+                        mWindowManager.updateViewLayout(mLinearLayout, paramsF);
                         break;
                 }
-                return false;
+                return true;
             }
         });
         return START_STICKY;
+    }
+
+    private void saveToDownloads() {
+        File dir = new File(outputFile);
+        DownloadManager downloadManager = (DownloadManager) JoystickService.this.getSystemService(DOWNLOAD_SERVICE);
+        if (downloadManager != null) {
+            downloadManager.addCompletedDownload(dir.getName(), dir.getName(), true, "video/3gpp", dir.getAbsolutePath(), dir.length(), true);
+        }
     }
 
     private void sendMessage() {
@@ -151,24 +175,37 @@ public class JoystickService extends Service {
                     != PackageManager.PERMISSION_GRANTED && ActivityCompat.
                     checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                     != PackageManager.PERMISSION_GRANTED) {
-                return;
+                sendMessageWithLocation(smsBody);
+            } else {
+                Location location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                smsBody.append("\nI'm here: \n");
+                smsBody.append("http://maps.google.com?q=");
+                smsBody.append(latitude);
+                smsBody.append(",");
+                smsBody.append(longitude);
+                sendMessageWithLocation(smsBody);
             }
-            Location location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            double latitude = location.getLatitude();
-            double longitude = location.getLongitude();
-            smsBody.append("\nI'm here: \n");
-            smsBody.append("http://maps.google.com?q=");
-            smsBody.append(latitude);
-            smsBody.append(",");
-            smsBody.append(longitude);
         } catch (Exception e) {
-            Toast.makeText(this, "Location not detected.", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
         }
+
+    }
+
+    private void sendMessageWithLocation(StringBuilder smsBody) {
         try {
             String numbers[] = phoneNumber.split(",");
             for (String number : numbers) {
-                SmsManager.getDefault().sendTextMessage(number.trim(), null,
-                        smsBody.toString(), null, null);
+                SmsManager sms = SmsManager.getDefault();
+                if (smsBody.length() < 160) {
+                    sms.sendTextMessage(number.trim(), null,
+                            smsBody.toString(), null, null);
+                } else {
+                    ArrayList<String> parts = sms.divideMessage(smsBody.toString());
+                    sms.sendMultipartTextMessage(number.trim(), null, parts,
+                            null, null);
+                }
             }
         } catch (Exception e) {
             Toast.makeText(this, "Message sent to invalid destination.", Toast.LENGTH_SHORT).show();
@@ -179,8 +216,13 @@ public class JoystickService extends Service {
     public void onDestroy() {
         super.onDestroy();
         if (mJoystickImageView != null) {
-            mWindowManager.removeView(mJoystickImageView);
+            //      mWindowManager.removeView(mJoystickImageView);
+            mWindowManager.removeView(mLinearLayout);
             mJoystickImageView = null;
+        }
+        if (myRecorder != null) {
+            myRecorder.release();
+            myRecorder = null;
         }
     }
 
@@ -193,16 +235,44 @@ public class JoystickService extends Service {
 
         @Override
         public void run() {
-            sendMessage();
+            if (ActivityCompat.checkSelfPermission(JoystickService.this, Manifest.permission.RECORD_AUDIO)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.
+                    checkSelfPermission(JoystickService.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return;
+            } else {
+                startRecording();
+            }
             while (flag) {
                 try {
+                    if (ActivityCompat.checkSelfPermission(JoystickService.this, Manifest.permission.SEND_SMS)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        flag = false;
+                    } else {
+                        sendMessage();
+                    }
                     Thread.sleep(10000);
-                    sendMessage();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             stopSelf(this.serviceId);
+        }
+
+        private void startRecording() {
+            Date date = new Date();
+            outputFile = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/" + date.toString() + "sp_audiorecords.3gp";
+            myRecorder = new MediaRecorder();
+            myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            myRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            myRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            myRecorder.setOutputFile(outputFile);
+            try {
+                myRecorder.prepare();
+                myRecorder.start();
+            } catch (IllegalStateException | IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- app/src/main/java/com/syezdsultanov/trevoga/MainActivity.java
+++ app/src/main/java/com/syezdsultanov/trevoga/MainActivity.java
@@ -13,9 +13,11 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,10 +27,11 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final int OVERLAY_PERMISSION_REQ_CODE = 111;
-    private static final int PICK_NUMBER_REQUEST = 222;
-    private static final int REQUEST_READ_CONTACTS = 333;
-    private static final int REQUEST_LOCATION = 444;
-    private static final int REQUEST_SEND_SMS = 555;
+    private static final int PICK_NUMBER_REQ_CODE = 222;
+    private static final int READ_CONTACTS_PERMISSION_REQ_CODE = 333;
+    private static final int LOCATION_PERMISSION_REQ_CODE = 444;
+    private static final int SEND_SMS_PERMISSION_REQ_CODE = 555;
+    private static final int RECORD_AUDIO_PERMISSION_REQ_CODE = 777;
     private EditText mSmsEditText, mContactsEditText;
     private StringBuilder numbers = new StringBuilder("");
     private String contactName, contactNumber;
@@ -56,6 +59,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 getSmsPermission();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (!Settings.canDrawOverlays(MainActivity.this)) {
+                        Toast.makeText(MainActivity.this, "Safety Plus needs Overlay permission", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                }
                 Intent starIntent = new Intent(MainActivity.this, JoystickService.class);
                 if (mContactsEditText.getText().toString().length() < 3) {
                     Toast.makeText(MainActivity.this, "Please enter a valid number.", Toast.LENGTH_SHORT).show();
@@ -83,8 +92,7 @@ public class MainActivity extends AppCompatActivity {
                     mSmsEditText.requestFocus();
                     count = 0;
                 }
-                getReadContactPermission();
-                pickContact();
+                getContactNumber();
             }
         });
 
@@ -95,8 +103,9 @@ public class MainActivity extends AppCompatActivity {
                 deleteContact();
             }
         });
-        checkPermission();
+        getOverlayPermission();
         getLocationPermission();
+        getRecordAudioPermission();
     }
 
     private void getLocationPermission() {
@@ -104,50 +113,63 @@ public class MainActivity extends AppCompatActivity {
                 != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                         != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQ_CODE);
         }
     }
 
     private void getSmsPermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, REQUEST_SEND_SMS);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SEND_SMS_PERMISSION_REQ_CODE);
         }
     }
 
     private void getReadContactPermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS},
-                    REQUEST_READ_CONTACTS);
+                    READ_CONTACTS_PERMISSION_REQ_CODE);
+        }
+    }
+
+    private void getRecordAudioPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RECORD_AUDIO_PERMISSION_REQ_CODE);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
         switch (requestCode) {
-            case REQUEST_READ_CONTACTS: {
-                if (grantResults.length < 0
+            case READ_CONTACTS_PERMISSION_REQ_CODE: {
+                if (grantResults.length > 0
                         && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Please, enter phone number manually.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Safety Plus needs to access contacts", Toast.LENGTH_LONG).show();
+                    finish();
                 }
                 return;
             }
-            case REQUEST_LOCATION: {
-                if (grantResults.length < 0
+            case LOCATION_PERMISSION_REQ_CODE: {
+                if (grantResults.length > 0
                         && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Location won't send to Emergency cotact", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Location will not sent to Emergency contact", Toast.LENGTH_SHORT).show();
                 }
                 return;
             }
-            case REQUEST_SEND_SMS: {
-                if (grantResults.length < 0
+            case SEND_SMS_PERMISSION_REQ_CODE: {
+                if (grantResults.length > 0
                         && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "SMS won't send to Emergency cotact", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Sms will not sent to Emergency contact.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            case RECORD_AUDIO_PERMISSION_REQ_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Audio will not recorded recorded.", Toast.LENGTH_SHORT).show();
                 }
             }
         }
     }
 
-    private void checkPermission() {
+    private void getOverlayPermission() {
         if (Build.VERSION.SDK_INT >= 23) {
             if (!Settings.canDrawOverlays(MainActivity.this)) {
                 Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
@@ -157,25 +179,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void pickContact() {
+    private void getContactNumber() {
+        getReadContactPermission();
         Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-        startActivityForResult(intent, PICK_NUMBER_REQUEST);
-
+        startActivityForResult(intent, PICK_NUMBER_REQ_CODE);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (resultCode == RESULT_CANCELED) {
-                }
+            if (!Settings.canDrawOverlays(this)) {
+                Log.d("TAG", "DENIYED");
+                finish();
             }
+
         }
-        if (requestCode == PICK_NUMBER_REQUEST && resultCode == RESULT_OK && data != null) {
+        if (requestCode == PICK_NUMBER_REQ_CODE && resultCode == RESULT_OK && data != null
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission
+                .READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
             Uri contactData = data.getData();
             Cursor contact = getContentResolver().query(contactData, null, null, null, null);
             if (contact.moveToFirst()) {
-                //contact name
                 contactName = contact.getString(contact.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
                 ContentResolver cr = getContentResolver();
                 Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null,
@@ -183,7 +208,6 @@ public class MainActivity extends AppCompatActivity {
                 if (cursor.moveToFirst()) {
                     String contactId =
                             cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-                    //  Get all phone numbers.
                     Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
                     while (phones.moveToNext()) {
@@ -203,7 +227,7 @@ public class MainActivity extends AppCompatActivity {
                 numbers.append(contactNumber);
             }
         }
-        if (requestCode == PICK_NUMBER_REQUEST && resultCode == RESULT_CANCELED) {
+        if (requestCode == PICK_NUMBER_REQ_CODE && resultCode == RESULT_CANCELED) {
             contactName = "";
         }
     }
